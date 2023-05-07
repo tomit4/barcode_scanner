c690ed3918b6045f9192f9b9cd58f0a91f6a4d88
tomit4 <mosssap@gmail.com>
2023-05-07 14:50:33 -0700

    :memo: Added sample output of dl scan

---
+++ barcode_sample_output.js
@@ -0,0 +1,29 @@
+[
+  { "@": "\u001e\rANSI 747954180002DL11401394DLDWANONE" }, // vehicle classes allowed to drive
+  { "@": "DCBB" }, // restrictions on driving privileges
+  { "@": "DCDNONE" }, // additional privileges granted to the cardholder
+  { "@": "DBA01012028" }, // expiration date of driver's license
+  { "@": "DCSDOE" }, // last name
+  { "@": "DACJOHN" }, // first name
+  { "@": "DADMIDDLENAME" }, // middle name(s)
+  { "@": "DBD01012022" }, // issue date of the document
+  { "@": "DBB01011977" }, // date of birth
+  { "@": "DBC1" }, // gender
+  { "@": "DAYBRO" }, // eye color
+  { "@": "DAU073 in" }, // height
+  { "@": "DAG9999 NE 19TH ST" }, // street name
+  { "@": "DAIPORTLAND" }, // city name
+  { "@": "DAJOR" }, // state name
+  { "@": "DAK98765432" }, // postal code
+  { "@": "DAQWDL65D7AD55C" }, // number of the cardholder
+  { "@": "DCFWDL65D7AD55CT089857522296" }, // number of the document
+  { "@": "DCGUSA" }, // country in which the document was issued
+  { "@": "DDEN" }, // indicator taht the last name is truncated
+  { "@": "DDFN" }, // indicator that the first name is truncated
+  { "@": "DDGN" }, // country in which the document was isssued
+  { "@": "DCJ5089857522296" }, // indicator that the middle names are truncated
+  { "@": "DDAN" }, // compliance type
+  { "@": "DDB01012019" }, // card revision date
+  { "@": "DAW200" }, // cardholder's weight in lbs
+  { "@": "DDK1\r" }, // organ donor indicator
+];
